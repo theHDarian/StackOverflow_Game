be2e232ec73a4e43e3d00dd1d94b0470ff1064ac
nekopudding <deany.y.ca@gmail.com>
2024-10-02 18:38:37 -0700

    improve input system by keeping track of last pressed movement axis

--- src/ecs/systems/world_system.cpp
+++ src/ecs/systems/world_system.cpp
@@ -256,7 +256,7 @@ void WorldSystem::movePlayer(vec2 inputAxis) {
 
 float WorldSystem::getModifiedValue(BulletEffectType bf, float value)
 {
-	return min(registry.stackCompile.get(player).minimums[bf], (value + registry.stackCompile.get(player).additives[bf]) * registry.stackCompile.get(player).multiplicatives[bf]);
+	return max(registry.stackCompile.get(player).minimums[bf], (value + registry.stackCompile.get(player).additives[bf]) * registry.stackCompile.get(player).multiplicatives[bf]);
 }
 
 
