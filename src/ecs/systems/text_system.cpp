cbd82ed5fee8cf5b57e2c20d6c17a9eae94ecee7
akt1nos <akt1nos@student.ubc.ca>
2024-10-07 16:50:59 -0700

    added text lib

---
+++ src/ecs/systems/text_system.cpp
@@ -0,0 +1,38 @@
+// ref: https://learnopengl.com/In-Practice/Text-Rendering
+
+#include "text_system.hpp" 
+
+struct Character {
+    unsigned int TextureID;  // ID handle of the glyph texture
+    glm::ivec2   Size;       // Size of glyph
+    glm::ivec2   Bearing;    // Offset from baseline to left/top of glyph
+    unsigned int Advance;    // Offset to advance to next glyph
+};
+
+std::map<char, Character> Characters;
+
+int initFreetypeLib() {
+    FT_Library ft;
+    if (FT_Init_FreeType(&ft))
+    {
+        std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+        return -1;
+    }
+
+    FT_Face face;
+    if (FT_New_Face(ft, font_path("pixelated.ttf").c_str(), 0, &face))
+    {
+        std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+        return -1;
+    }
+
+    FT_Set_Pixel_Sizes(face, 0, 48);
+
+    if (FT_Load_Char(face, 'X', FT_LOAD_RENDER))
+    {
+        std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+        return -1;
+    }
+
+    return 1;
+}
---
+++ src/ecs/systems/text_system.hpp
@@ -0,0 +1,7 @@
+#include <ft2build.h>
+#include FT_FREETYPE_H  
+#include <iostream>
+#include "common.hpp"
+#include <map>
+
+int initFreetypeLib();
