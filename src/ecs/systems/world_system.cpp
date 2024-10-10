a1f0aac1eac095502ec17b4f9ae6ccce8e112a68
Sky Huang <cwlsky@student.ubc.ca>
2024-10-09 21:07:12 -0700

    added player turning logic

--- src/ecs/systems/world_system.cpp
+++ src/ecs/systems/world_system.cpp
@@ -460,7 +460,6 @@ void WorldSystem::dash(vec2 direction, float elapsed_ms_since_last_update) {
 void WorldSystem::shoot(float elapsed_ms_since_last_update, int cluster) {
 	IOState& input = registry.ioStates.components[0];
 	Shoots& pl = registry.shoots.get(player);
-	Player& pl = registry.players.get(player);
     Motion& player_motion = registry.motions.get(player);
     vec2 playerPos = player_motion.position;
     vec2 bulletDir = glm::normalize(input.mousePosition - player_motion.position);
