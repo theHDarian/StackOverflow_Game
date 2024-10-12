eb2954bb2a8f00bf4681c892eb5b130e1918d64d
vincentgilbert <vingilbe@students.cs.ubc.ca>
2024-10-12 12:58:12 -0700

    Finalize movement

--- src/ecs/systems/world_system.cpp
+++ src/ecs/systems/world_system.cpp
@@ -231,7 +231,7 @@ bool WorldSystem::step(float elapsed_ms_since_last_update) {
 
 	WindowState& wS = registry.windowStates.components[0];
 	if (registry.enemies.size() == 0) {
-		createEnemy(renderer, vec2(wS.width * uniformDist(rng),wS.height * uniformDist(rng)), vec2(0, 0));
+		createEnemy(renderer, vec2(wS.width * uniformDist(rng),wS.height * uniformDist(rng)), vec2(0, 0), EnemyAttackPattern::ALL_DIRECTION, EnemyBehavior::FOLLOW_PLAYER);
 	}
 
 	// Processing the salmon state
