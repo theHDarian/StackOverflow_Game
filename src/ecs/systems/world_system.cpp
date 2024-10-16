e8204b63888ece660cfb50ab6d230838a5e904f3
akt1nos <akt1nos@students.cs.ubc.ca>
2024-10-16 00:18:17 -0700

    Reverted back to full screen mode for stable refresh rate

--- src/ecs/systems/world_system.cpp
+++ src/ecs/systems/world_system.cpp
@@ -86,12 +86,12 @@ GLFWwindow* WorldSystem::createWindow() {
 	const GLFWvidmode* vidMode = glfwGetVideoMode(monitor);
 	window_width_px = vidMode->width;
 	window_height_px = vidMode->height;
-	//window = glfwCreateWindow(window_width_px, window_height_px, "StackOverflow", monitor, nullptr);
+	window = glfwCreateWindow(window_width_px, window_height_px, "StackOverflow", monitor, nullptr);
 
 	// FOR DEBUGGING AT SMALLER WINDOW SIZES
 	// window_width_px = 1280;
 	// window_height_px = 720;
-	 window = glfwCreateWindow(window_width_px, window_height_px, "StackOverflow", nullptr, nullptr);
+	 //window = glfwCreateWindow(window_width_px, window_height_px, "StackOverflow", nullptr, nullptr);
 
 	Entity ent = Entity();
 	WindowState& windowState = registry.windowStates.emplace(ent);
