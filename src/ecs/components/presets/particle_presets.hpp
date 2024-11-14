25d7ef7bf54dfd650b6ca87e45d737c4e1aedb83
nekopudding <deany.y.ca@gmail.com>
2024-11-14 02:49:24 -0800

    use presets

--- src/ecs/components/presets/particle_presets.hpp
+++ src/ecs/components/presets/particle_presets.hpp
@@ -8,7 +8,7 @@
 
 // struct ParticleProps {
 //     Vec2Randomized position = {{0,0},{0,0}};
-//     Vec2Randomized velocity = {{0,0},{10,10}};
+//     Vec2Randomized velocity = {{0,0},{100,100}};
 
 //     Vec4StartEnd color = {{ 143 / 255.0f, 37 / 255.0f, 132 / 255.0f, 1.0f },{ 17 / 255.0f, 14 / 255.0f, 99 / 255.0f, .4f }}; //begin, end
 //     FloatStartEndRandomized size = {10.f,2.f,3.f}; //begin, end, variation
@@ -17,9 +17,10 @@
 //     float lifetime = 1000.0f;
 // };
 
+
 const ParticleProps playerTrail {
     {{0,0},{0,0}},
-    {{0,0},{10,10}},
+    {{0,0},{100,100}},
     {{ 39 / 255.0f, 217 / 255.0f, 95 / 255.0f, .7f },{ 42 / 255.0f, 124 / 255.0f, 133 / 255.0f, .4f }},
     {10.f,2.f,3.f},
     -1,
@@ -28,7 +29,7 @@ const ParticleProps playerTrail {
 
 const ParticleProps sparks {
     {{0,0},{0,0}},
-    {{0,0},{10,10}},
+    {{0,0},{100,100}},
     {{ 254 / 255.0f, 212 / 255.0f, 123 / 255.0f, 1.0f },{ 254 / 255.0f, 109 / 255.0f, 41 / 255.0f, 1.0f }},
     {10.f,2.f,3.f},
     -1,
