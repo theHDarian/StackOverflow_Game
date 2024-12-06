e83575ca9165defe72a32d7d31e942ad6f47ad0c
Tom <yelf42@gmail.com>
2024-12-05 19:34:13 -0800

    glitch shader

--- src/shaders/animate.fs.glsl
+++ src/shaders/animate.fs.glsl
@@ -15,20 +15,34 @@ uniform bool glitchToggle = false;
 uniform float time;
 uniform sampler2DArray glitchMask;
 uniform sampler2DArray glitch;
+float glitchOffset = 0.05;
 
 // Output color
 layout(location = 0) out  vec4 color;
 
-float rand(vec2 co) {
-	return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
+vec2 clampedGlitchOffset(vec2 offset) {
+	return clamp(texcoord + offset, vec2(0.0), vec2(1.0));
 }
 
 void main()
 {
+
 	color = vec4(fcolor, alpha) * texture(sampler0, vec3(texcoord.x, texcoord.y, frame));
 	if (glitchToggle) {
-		float a = texture(glitchMask, vec3(texcoord.x, texcoord.y, floor(64.0 * rand(vec2(sin(time)))))).a;
-		if (a > 0.5) color = texture(glitch, vec3(texcoord.x, texcoord.y, floor(39.0 * rand(vec2(sin(time))))));
+		float a = texture(glitchMask, vec3(texcoord.x, texcoord.y, floor(64.0 * mod(0.01 * time, 1)))).a;
+		if (a > 0.5 && (
+						texture(sampler0, vec3(clampedGlitchOffset(vec2(0.0)), frame)).a > 0.0			||
+						texture(sampler0, vec3(clampedGlitchOffset(vec2(glitchOffset)), frame)).a > 0.0		||
+						texture(sampler0, vec3(clampedGlitchOffset(vec2(glitchOffset, 0.0)), frame)).a > 0.0	||
+						texture(sampler0, vec3(clampedGlitchOffset(vec2(glitchOffset, -glitchOffset)), frame)).a > 0.0||
+						texture(sampler0, vec3(clampedGlitchOffset(vec2(0.0, -glitchOffset)), frame)).a > 0.0	||
+						texture(sampler0, vec3(clampedGlitchOffset(vec2(-glitchOffset)), frame)).a > 0.0		||
+						texture(sampler0, vec3(clampedGlitchOffset(vec2(-glitchOffset, 0.0)), frame)).a > 0.0	||
+						texture(sampler0, vec3(clampedGlitchOffset(vec2(-glitchOffset, glitchOffset)), frame)).a > 0.0||
+						texture(sampler0, vec3(clampedGlitchOffset(vec2(0.0, glitchOffset)), frame)).a > 0.0
+					   )) {
+			color = texture(glitch, vec3(texcoord.x, texcoord.y, floor(39.0 * mod(0.02 * time, 1))));
+		}
 	}
 	// note: branches are expensive, consider using another shader instead?
 	if (changeColor == 1){
