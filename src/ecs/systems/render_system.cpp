b688ecf9eb50c128da09b0c1729a4a7bfaaefa51
Sky_desktop <cwlsky@student.ubc.ca>
2025-01-04 00:53:51 -0800

    Added glitch shader,currently not applying correctly to room symbols

--- src/ecs/systems/render_system.cpp
+++ src/ecs/systems/render_system.cpp
@@ -294,9 +294,10 @@ void RenderSystem::drawTexturedMesh(Entity entity,
 			if (registry.doorSymbols.has(entity))
             {
 				for (Entity& d : registry.doors.entities) {
-					if (registry.doors.get(d).side == registry.doorSymbols.get(entity).side && registry.doors.get(d).preset.hasElite) {
+					if ((registry.doors.get(d).side == registry.doorSymbols.get(entity).side) && registry.doors.get(d).preset.hasElite) {
+						std::cout << "symbol: " << registry.doorSymbols.get(entity).side << " door: " << registry.doors.get(d).side << ", preset "<< registry.doors.get(d).preset.ID << std::endl;
 						should_glitch = true;
-                        break;
+						break;
                     }
 				}
             }
