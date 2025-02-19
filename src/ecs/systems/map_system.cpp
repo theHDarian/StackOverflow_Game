2436f621e57c512ff878e7f4f73364d397adda65
Tom <yelf42@gmail.com>
2025-02-19 12:40:12 -0800

    Mining and Medical floor addons

--- src/ecs/systems/map_system.cpp
+++ src/ecs/systems/map_system.cpp
@@ -437,7 +437,7 @@ void MapSystem::decorateRoom() {
     // Create floor decorations
     Map& map = registry.maps.components[0];
     WindowState& ws = registry.windowStates.components[0];
-    std::string filename = (map.currRegion == Biology) ? "bio_floor_addons" : (map.currRegion == Physics) ? "hifi_floor_addons" : "tutorial_floor_addons";
+    std::string filename = (map.currRegion == Biology) ? "bio_floor_addons" : (map.currRegion == Physics) ? "hifi_floor_addons" : (map.currRegion == Mining) ? "mining_floor_addons" : (map.currRegion == Medical) ? "medical_floor_addons" : "tutorial_floor_addons";
     vec2 placements = vec2(floor(0.8 * map.currRoom.preset.roomSize.x / 192.f), floor( 0.8 * map.currRoom.preset.roomSize.y / 192.f));
     vec2 dividers = vec2(0.9090 * map.currRoom.preset.roomSize.x / placements.x, 0.9090 * map.currRoom.preset.roomSize.y / placements.y);
     vec2 roomOffset = vec2(-map.currRoom.preset.roomSize.x / 2.2f, -map.currRoom.preset.roomSize.y / 2.2f) + vec2(ws.width, ws.height) / 2.f;
